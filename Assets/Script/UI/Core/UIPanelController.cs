835be5b83777e09bb86941a0616155ed7eb6c6cb
moonsoyi <alsgur7949@naver.com>
2025-09-19 15:11:41 +0900

    SetttingPanel프리팹 추가
    UITestScene에서 SetttingPanel프리팹 추가
    Master, BGM, Effect 관련 UI 추가
     - 슬라이더, 토글(토글 텍스트) 추가

---
+++ Assets/Script/UI/Core/UIPanelController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIPanelController : MonoBehaviour
+{
+    // TODO: Button - Panel 연결 내용 추가 필요, Panel에서 Regist하는 부분은 있다. 아마도 안정성을 위해서 IUIPanel 이용하는 함수로 사용
+    // 이를 각 타입과 함수를 매핑하여서, 싱글턴에서 사용하는 방식과 개별 방식 생각 중
+    // 추가로 panelId를 사용하는 것 같은데, 실제로 사용가능한지에 대해서 확인
+
+    // Singleton 패턴
+    private static UIPanelController instance;
+    public static UIPanelController Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<UIPanelController>();
+
+                if (instance == null)
+                {
+                    var controllerGO = new GameObject("UIPanelController");
+                    instance = controllerGO.AddComponent<UIPanelController>();
+                    DontDestroyOnLoad(controllerGO);
+                    Debug.Log("UIPanelManager 자동 생성됨");
+                }
+            }
+            return instance;
+        }
+    }
+    private void Awake()
+    {
+        // Singleton 패턴 구현
+        if (instance != null && instance != this)
+        {
+            Debug.LogWarning("UIPaneController 중복 인스턴스 제거");
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
